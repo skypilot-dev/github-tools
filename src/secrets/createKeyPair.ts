231b5a5b5fb166a99cba718a354783deacc3585b
William Thorsen <william@thorsen.dev>
2020-02-11 12:21:49 -0700

    internal: Can create a public/secret key pair
    Added `createKeyPair` function

---
+++ src/secrets/createKeyPair.ts
@@ -0,0 +1,25 @@
+/* Reference:
+ * https://github.com/tonyg/js-nacl#anonymous-authenticated-encryption-crypto_box_seal
+ * */
+
+import util from 'util';
+import NaclFactory, { BoxKeyPair } from 'js-nacl';
+
+export type Utf8KeyPair = {
+  publicKey: Uint8Array;
+  secretKey: Uint8Array;
+}
+
+const instantiate = util.promisify(NaclFactory.instantiate);
+
+export async function createKeyPair(): Promise<Utf8KeyPair> {
+  return new Promise(resolve => {
+    instantiate(nacl => {
+      const keyPair: BoxKeyPair = nacl.crypto_box_keypair();
+      resolve({
+        publicKey: keyPair.boxPk,
+        secretKey: keyPair.boxSk,
+      });
+    });
+  })
+}
